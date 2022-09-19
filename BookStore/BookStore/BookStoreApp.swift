84a76d6ced99c2fd20ed8be94b2ca9129cb9f003
Siwon Kim <siyeon0616@naver.com>
2022-09-19 18:57:52 +0900

    feat: 프로젝트 생성

---
+++ BookStore/BookStore/BookStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookStoreApp.swift
+//  BookStore
+//
+//  Created by Siwon Kim on 2022/09/19.
+//
+
+import SwiftUI
+
+@main
+struct BookStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
