6522e54564b8e4962deee4315507156eb3f0ddad
Siwon Kim <siyeon0616@naver.com>
2022-09-20 08:41:39 +0900

    feat: SearchViewModel에 API 호출을 위한 getBookList 생성
    - keyword와 books를 @Published 로 두어서 View에서 변화를 감지할 수 있도록 함

--- BookStore/BookStore/Presentation/ViewModel/SearchViewModel.swift
+++ BookStore/BookStore/Presentation/ViewModel/SearchViewModel.swift
@@ -8,15 +8,32 @@
 import Foundation
 import Combine
 
-final class SearchViewModel: ObservableObject {
+final class SearchViewModel: ObservableObject, Identifiable {
+    @Published var keyword: String = ""
+    @Published var books: [Book] = []
+    
+    private var disposables = Set<AnyCancellable>()
     private let searchUseCase: SearchUseCaseType
     
     init(searchUseCase: SearchUseCaseType) {
         self.searchUseCase = searchUseCase
     }
     
-    func getBookList(with keyword: String) -> AnyPublisher<BookList, Error> {
-        return searchUseCase.getBookList(with: keyword)
+    func getBookList(with keyword: String) {
+        searchUseCase.getBookList(with: keyword)
+            .map { $0.books }
+            .receive(on: DispatchQueue.main)
+            .sink { value in
+                switch value {
+                case let .failure(error):
+                    print(error)
+                case .finished:
+                    break
+                }
+            } receiveValue: { [weak self] books in
+                self?.books = books
+            }
+            .store(in: &disposables)
     }
     
 }
