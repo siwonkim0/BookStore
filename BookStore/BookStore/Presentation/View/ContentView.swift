00c997932f26b5c8bf557035d357066c216e0d8b
Siwon Kim <siyeon0616@naver.com>
2022-09-20 13:53:26 +0900

    feat: ViewModel을 ContentView에 environmentObject로 주입 #3

---
+++ BookStore/BookStore/Presentation/View/ContentView.swift
@@ -0,0 +1,15 @@
+//
+//  ContentView.swift
+//  BookStore
+//
+//  Created by Siwon Kim on 2022/09/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        SearchView()
+    }
+}
+
