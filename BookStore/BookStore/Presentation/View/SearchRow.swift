1f064d309625857021572bd5de4a7828aca9decd
Siwon Kim <siyeon0616@naver.com>
2022-09-21 11:11:29 +0900

    feat: SearchView의 검색 결과를 List로 변경 #3
    - Grid는 Book의 title, information, price 모두를 표현하기에 부적절하다고 판단하여 list로 다시 변경함

---
+++ BookStore/BookStore/Presentation/View/SearchRow.swift
@@ -0,0 +1,48 @@
+//
+//  SearchRow.swift
+//  BookStore
+//
+//  Created by Siwon Kim on 2022/09/21.
+//
+
+import Foundation
+import SwiftUI
+
+struct SearchRow: View {
+    var book: Book
+    
+    var body: some View {
+        HStack {
+            AsyncImage(url: URL(string: book.image)) { phase in
+                if let image = phase.image {
+                    image
+                        .resizable()
+                        .clipped()
+                        .aspectRatio(contentMode: .fill)
+                } else if phase.error != nil {
+                    Color.gray
+                } else {
+                    ProgressView()
+                        
+                }
+            }.padding(.trailing).frame(width: 70, height: 120)
+            VStack(alignment: .leading) {
+                Text(book.title)
+                    .font(.subheadline)
+                    .bold()
+                Text(book.subtitle)
+                    .font(.caption)
+                    .foregroundColor(Color.gray)
+                Text(book.price)
+            }
+        }
+        
+    }
+}
+
+struct SearchRow_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchRow(book: Book(id: UUID(), title: "previewTitle", subtitle: "previewSubtitle", isbn13: "123", price: "100", image: "", url: "https://itbook.store/books/9781783988006"))
+            .previewLayout(.fixed(width: 300, height: 70))
+    }
+}
