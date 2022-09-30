21e02eaf990bf4eeea175576b3b1d086da9f9dab
Siwon Kim <siyeon0616@naver.com>
2022-10-01 00:08:09 +0900

    feat: BookDetailView에서 링크를 누르면 웹뷰를 모달로 보여주도록 구현 #8

--- BookStore/BookStore/Presentation/Detail/View/BookDetailView.swift
+++ BookStore/BookStore/Presentation/Detail/View/BookDetailView.swift
@@ -8,13 +8,39 @@
 import SwiftUI
 
 struct BookDetailView: View {
+    @State private var isPresented: Bool = false
+    @Binding var isMemo: Bool
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            VStack {
+                Image(systemName: "character.book.closed.fill")
+                    .resizable()
+                    .frame(width: 150, height: 200, alignment: .center)
+                    .foregroundColor(.cyan)
+                    .padding(.bottom)
+                Text("The NGINX Real-Time API Handbook")
+                    .font(.title.bold())
+                    .padding(.bottom)
+                Text("Our NGINX Real-Time API Handbook equips enterprises to deliver reliable, high-performance APIs")
+                Text("isbn: 1001650120608")
+                Text("price: $13.99")
+                Button("link") {
+                    isPresented = true
+                }
+                .sheet(isPresented: $isPresented) { //모달창이 내려가면 자동으로 false
+                    WebView(url: URL(string: "https://itbook.store/books/9781849517744")!)
+                }
+            }
+            .padding(.all)
+            .frame(alignment: .leading)
+        }
+        
     }
 }
 
 struct BookDetailView_Previews: PreviewProvider {
     static var previews: some View {
-        BookDetailView()
+        BookDetailView(isMemo: .constant(true))
     }
 }
