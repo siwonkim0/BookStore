cf27cdf700d8914ff94f1b8b5eb4feefec887a8e
Siwon Kim <siyeon0616@naver.com>
2022-10-01 12:40:42 +0900

    feat: BookDetailView에 TextEditor 생성 #8
    - body에 있던 View extension으로 분리함

--- BookStore/BookStore/Presentation/Detail/View/BookDetailView.swift
+++ BookStore/BookStore/Presentation/Detail/View/BookDetailView.swift
@@ -8,34 +8,65 @@
 import SwiftUI
 
 struct BookDetailView: View {
-    @State private var isPresented: Bool = false
+    @State private var isWebViewPresented: Bool = false
     @Binding var isMemo: Bool
+    @State private var memoText: String = "sds"
     
     var body: some View {
         ScrollView {
             VStack {
-                Image(systemName: "character.book.closed.fill")
-                    .resizable()
-                    .frame(width: 150, height: 200, alignment: .center)
-                    .foregroundColor(.cyan)
-                    .padding(.bottom)
-                Text("The NGINX Real-Time API Handbook")
-                    .font(.title.bold())
-                    .padding(.bottom)
+                imageView
+                titleView
                 Text("Our NGINX Real-Time API Handbook equips enterprises to deliver reliable, high-performance APIs")
                 Text("isbn: 1001650120608")
                 Text("price: $13.99")
+
                 Button("link") {
-                    isPresented = true
+                    isWebViewPresented = true
                 }
-                .sheet(isPresented: $isPresented) { //모달창이 내려가면 자동으로 false
+                .sheet(isPresented: $isWebViewPresented) { //모달창이 내려가면 자동으로 false
                     WebView(url: URL(string: "https://itbook.store/books/9781849517744")!)
                 }
+                descriptionView
+                textEditorView
+                submitButton
             }
             .padding(.all)
-            .frame(alignment: .leading)
         }
         
+    }
+}
+
+private extension BookDetailView {
+    var imageView: some View {
+        Image(systemName: "character.book.closed.fill")
+            .resizable()
+            .frame(width: 150, height: 200, alignment: .center)
+            .foregroundColor(.cyan)
+            .padding(.bottom)
+    }
+    
+    var titleView: some View {
+        Text("The NGINX Real-Time API Handbook")
+            .font(.title.bold())
+            .padding(.bottom)
+    }
+    
+    var textEditorView: some View {
+        TextEditor(text: $memoText)
+            .frame(height: 250)
+            .padding(.all, .maximum(5, 5))
+            .overlay(RoundedRectangle(cornerRadius: 10).stroke(.gray))
+    }
+    
+    var descriptionView: some View {
+        Text("you can take memo here")
+    }
+    
+    var submitButton: some View {
+        Button("save") {
+            print("aaaa")
+        }
     }
 }
 
